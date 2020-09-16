e65aa58e3c8c667fe45ee366ae13500d5a2a8eba
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-15 21:34:03 -0400

    Added two rectangles

--- Experiments/Week 2 - drawing experiments/js/script.js
+++ Experiments/Week 2 - drawing experiments/js/script.js
@@ -9,6 +9,16 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
+
+  background(200,200,200);
+
+  rectMode(CENTER);
+  rect(250,250,50,50);
+
+  rectMode(CORNER);
+  rect(250,250,50,50);
+
 
 }
 
