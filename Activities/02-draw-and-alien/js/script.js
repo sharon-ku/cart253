6c67cfde26abb8b424e4866c7e0a7c60aa54d6b0
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-16 13:00:32 -0400

    Alien: added body, head, and eyes

--- Activities/02-draw-and-alien/js/script.js
+++ Activities/02-draw-and-alien/js/script.js
@@ -1,14 +1,36 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 2: Draw an alien
+Sharon Ku
 
-Here is a description of this template p5 project.
+Drawing a friendly alien whose name is Bobby.
 **************************************************/
 
 // setup()
 //
-// Description of setup() goes here.
+// Drawing the alien.
 function setup() {
+
+// Setting up the canvas & coloring the background
+createCanvas(640,480);
+background(247, 198, 239);
+
+// Drawing the body
+noStroke();
+fill(191, 191, 191);
+ellipse(320,300,200,200);
+
+// Drawing the head
+fill(128, 128, 128);
+ellipse(320,150,150,200);
+
+// Drawing the eyes
+fill(0);
+ellipse(245,130,60,90); //leftie
+ellipse(395,130,60,90); //rightie
+
+
+
+
 
 }
 
