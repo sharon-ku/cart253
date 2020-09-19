e7e27b52166d7ccd0df1b0c89b077f1ba1fb9e0d
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-19 16:59:54 -0400

    3.4 introducing javascript objects

--- Experiments/Week 3 - variable experiments/3.2 creating variables/js/script.js
+++ Experiments/Week 3 - variable experiments/3.2 creating variables/js/script.js
@@ -4,25 +4,32 @@ Sharon Ku
 
 3.2 Creating variables
 3.3 Changing variables
+3.4 Introducing JavaScript objects
 **************************************************/
 
 let bgShade = 0;
+
+/** previous way of writing the variables
 let circleX = 0;
 let circleY = 250;
 let circleSize = 100;
 let circleSpeed = 2;
-let circleAcceleration =+ 0.25;
+let circleAcceleration = 0.25;
+**/
+
+
+let circle = {
+  x:0,
+  y:250,
+  size:100,
+  speed:2
+};
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
   createCanvas(500, 500);
-
-
-
-
 }
 
 // draw()
@@ -31,11 +38,11 @@ function setup() {
 function draw() {
   // bgShade = bgShade + 1;
   background(bgShade);
-  circleX += circleSpeed;
-  circleSpeed += circleAcceleration;
+  circle.x += circle.speed;
+  //circleSpeed += circleAcceleration;
   // circleSize = circleSize * 1.01;
   // circleY = circleY / 1.01;
-  ellipse(circleX,circleY,circleSize);
+  ellipse(circle.x,circle.y,circle.size);
 
 
 }
