fe6468dc11645b91e3220c5854083c6f708462f4
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-19 16:43:16 -0400

    W3.2 Learned about changing variables!!

--- Experiments/Week 3 - variable experiments/3.2 creating variables/js/script.js
+++ Experiments/Week 3 - variable experiments/3.2 creating variables/js/script.js
@@ -3,12 +3,15 @@ Week 3 variable experiments
 Sharon Ku
 
 3.2 Creating variables
+3.3 Changing variables
 **************************************************/
 
 let bgShade = 0;
-let circleX = 250;
+let circleX = 0;
 let circleY = 250;
 let circleSize = 100;
+let circleSpeed = 2;
+let circleAcceleration =+ 0.25;
 
 // setup()
 //
@@ -26,8 +29,13 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-
+  // bgShade = bgShade + 1;
   background(bgShade);
+  circleX += circleSpeed;
+  circleSpeed += circleAcceleration;
+  // circleSize = circleSize * 1.01;
+  // circleY = circleY / 1.01;
   ellipse(circleX,circleY,circleSize);
+
 
 }
