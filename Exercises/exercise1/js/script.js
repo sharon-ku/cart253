cd093cb8c9cf937c9f863c76e97a0d805f814b37
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-20 16:52:52 -0400

    E1: Created 3 circles + animated their colors (changes based on mouse position)

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -1,20 +1,62 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Exercise 1: I like to move it move it!
+Sharon Ku
 
 Here is a description of this template p5 project.
 **************************************************/
+
+
+
+let circleFill = {
+  r:172,
+  g:172,
+  b:172,
+}
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
+
+  //Circle 1
+      //Animating the color of the circle based on mouse's position
+      circleFill.r = map(mouseX, 0, windowWidth, 150, 255);
+      circleFill.g = map(mouseX, 0, windowWidth, 200, 255);
+      circleFill.b = 172;
+      fill(circleFill.r, circleFill.g, circleFill.b);
+
+      ellipse(100,100,100,100);
+
+  //Circle 2
+      //Animating the color of the circle based on mouse's position
+      circleFill.r = map(mouseY, windowHeight, 0, 200, 255);
+      circleFill.g = 172;
+      circleFill.b = map(mouseY, windowHeight, 0, 150, 255);
+      fill(circleFill.r, circleFill.g, circleFill.b);
+
+      ellipse(500,500,100,100);
+
+
+  //Circle 3
+      //Animating the color of the circle based on mouse's position
+      circleFill.r = 172;
+      circleFill.g = map(mouseX, 0, windowWidth, 150, 255);
+      circleFill.b = map(mouseY, windowHeight, 0, 200, 255);
+      fill(circleFill.r, circleFill.g, circleFill.b);
+
+      ellipse(700,100,100,100);
+
+
+
+
 
 }
