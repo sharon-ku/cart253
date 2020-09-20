f12ee1f4cd6c066ae9da8701bd813ced7150e9d2
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-20 14:42:59 -0400

    A3: finished animating the background (goes from black to red)

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -1,20 +1,50 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 3: Moving Pictures
+Sharon Ku
 
-Here is a description of this template p5 project.
+Two circles, the left one bigger and more transparent than the right, come in from either side of the screen, growing as they do so. They stop in the centre while still growing. The background goes from black to red.
 **************************************************/
+
+//Setting up my variables
+let bgColor = {
+  red:0,
+  green:0,
+  blue:0,
+  redChangeRate:0.5,
+  redMin:0,
+  redMax:255,
+}
+
+let bigCircle = {
+  x: 0,
+  y: 250,
+  size: 200,
+}
+
 
 // setup()
 //
-// Description of setup() goes here.
+//Description of setup
 function setup() {
 
+  createCanvas(500,500);
+  noStroke();
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+
+  //Background changing from black to red
+  bgColor.red += bgColor.redChangeRate;
+  bgColor.red = constrain(bgColor.red,bgColor.redMin,bgColor.redMax);
+  background(bgColor.red,bgColor.green,bgColor.blue);
+
+
+
+
+  ellipse(bigCircle.x, bigCircle.y, bigCircle.size);
+
 
 }
