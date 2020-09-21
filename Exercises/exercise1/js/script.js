85ee0b66e9ef68e7e7ef9f7f073340f7488c7ac8
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-20 20:57:40 -0400

    E1: Set constraints to circle 1's position (cannot surpass canvas boundaries)

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -5,7 +5,7 @@ Sharon Ku
 Here is a description of this template p5 project.
 **************************************************/
 
-
+let maxDistance = 100; //max distance between two points on the canvas
 
 let circleFill = {
   r: 172,
@@ -16,10 +16,15 @@ let circleFill = {
 let circle1 = {
   x: 100,
   y: 100,
+  xMin: 0,
+  xMax: 100,
+  yMin: 0,
+  yMax: 100,
   speed: 0.008,
   acceleration: 4,
   sizeMin: 100,
   sizeMax: 400,
+  distMouse: 100,
 }
 
 let circle2 = {
@@ -27,6 +32,8 @@ let circle2 = {
   y: 500,
   speed: 0.006,
   acceleration: 3,
+  sizeMin: 100,
+  sizeMax: 600,
 }
 
 let circle3 = {
@@ -45,6 +52,10 @@ let circle3 = {
 function setup() {
   createCanvas(windowWidth,windowHeight);
   noStroke();
+
+  //calculating the diagonal distance from one corner of the canvas to the other corner
+  maxDistance = dist(0, 0, windowWidth, windowHeight);
+  console.log(windowWidth, windowHeight, maxDistance);
 }
 
 // draw()
@@ -60,19 +71,29 @@ function draw() {
       circleFill.b = 172;
       fill(circleFill.r, circleFill.g, circleFill.b);
 
+
+
+
+
+      //Animating the circle's size
+      circle1.distMouse = dist(mouseX, mouseY, circle1.x, circle1.y);
+      circle1.size = map(circle1.distMouse, 0, maxDistance, circle1.sizeMin, circle1.sizeMax); //  NEED TO CHANGE WINDOWWIDTH
+
+
       // Animating the circle's movement
+      circle1.xMin = circle1.size / 2;
+      circle1.xMax = windowWidth-(circle1.size / 2);
+      circle1.yMin = circle1.size / 2;
+      circle1.yMax = windowHeight-(circle1.size / 2);
+
+      circle1.x = constrain(circle1.x, circle1.xMin, circle1.xMax);
+      circle1.y = constrain(circle1.y, circle1.yMin, circle1.yMax);
+
       circle1.x += (mouseX - circle1.x) * circle1.speed * circle1.acceleration;
       circle1.y += (mouseY - circle1.y) * circle1.speed * circle1.acceleration;
-
-      //Animating the circle's size
-      circle1.size = map(dist(mouseX, mouseY, circle1.x, circle1.y), 0, windowWidth, circle1.sizeMin, circle1.sizeMax); //  NEED TO CHANGE WINDOWWIDTH
-
-
-
+      
       // Drawing the circle
       ellipse(circle1.x, circle1.y, circle1.size);
-
-
 
 
 
