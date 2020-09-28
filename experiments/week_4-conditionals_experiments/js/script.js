04bad82c7b2985941da1850c7e9fb1640da4ccab
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-28 12:39:59 -0400

    W4: orbit example using rotate()

--- experiments/week_4-conditionals_experiments/js/script.js
+++ experiments/week_4-conditionals_experiments/js/script.js
@@ -6,44 +6,25 @@ Here is a description of this template p5 project.
 **************************************************/
 
 
-let circle = {
-  x: 250,
-  y: 250,
-  size: 100,
-  vx: 1,
-  vy: 0,
-  ax: 0,
-  ay: 0,
-  speed: 5,
-  acceleration: 0.1,
-  maxSpeed: 10,
-};
+let angle1 = 0;
+let angle2 = 0;
 
-let angle = 0;
-let rectScale = 0;
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
   background(0);
 
-
-  push();
-  fill(255,0,0);
+  translate(250, 250);
+  rotate(angle1);
   rectMode(CENTER);
-  translate(width/2, height/2);
-  rotate(angle);
-  scale(rectScale);
-  rect(0,0,100,100);
-  pop();
+  rect(0, 0, 100, 100);
 
-  angle = angle + 0.01;
-  rectScale = rectScale + 0.01;
+  translate(100, 0);
+  rotate(angle2)
+  rect(0, 0, 50, 50);
+
+  angle1 = angle1 + 0.01;
+  angle2 = angle2 + 0.02;
 }
