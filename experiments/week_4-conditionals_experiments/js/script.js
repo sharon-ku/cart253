ab8893182ea2350b934b3660bef81a77f7b4b1b9
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-28 12:48:20 -0400

    W4: 3D rotating cube shape

--- experiments/week_4-conditionals_experiments/js/script.js
+++ experiments/week_4-conditionals_experiments/js/script.js
@@ -6,25 +6,45 @@ Here is a description of this template p5 project.
 **************************************************/
 
 
-let angle1 = 0;
-let angle2 = 0;
+// Angles of rotation for our shape
+let angleX = 0;
+let angleY = 0;
 
 function setup() {
-  createCanvas(500, 500);
+  // Using WEBGL in createCanvas to specify 3D graphics
+  createCanvas(500, 500, WEBGL);
 }
 
 function draw() {
   background(0);
 
-  translate(250, 250);
-  rotate(angle1);
-  rectMode(CENTER);
-  rect(0, 0, 100, 100);
+  // Our shape
+  push();
+  // Translate to the center (not really needed, but just for completeness)
+  translate(0, 0, 0);
+  // Rotate AROUND the x axis
+  rotateX(angleX);
+  // Rotate AROUND the y axis
+  rotateY(angleY);
+  // Looks nicer
+  noStroke();
+  // Our central cube is white
+  fill(255);
+  box(100);
+  // A red bar passing through the box
+  fill(255, 0, 0);
+  box(200, 25, 25);
+  // A green bar passing through the box
+  fill(0, 255, 0);
+  box(25, 200, 25);
+  // A blue bar passing through the box
+  fill(0, 0, 255);
+  box(25, 25, 200);
+  // Note how the entire shape rotates because the rotateX() and rotateY() are applied to everything
+  // afterwards until the pop() below here
+  pop();
 
-  translate(100, 0);
-  rotate(angle2)
-  rect(0, 0, 50, 50);
-
-  angle1 = angle1 + 0.01;
-  angle2 = angle2 + 0.02;
+  // Increase the angles to rotate over time
+  angleX = angleX + 0.01;
+  angleY = angleY + 0.05;
 }
