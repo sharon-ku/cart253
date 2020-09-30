74aa2052287ff7aa72217cf575566b5491b9a7ca
sharon-ku <sharon-ku@users.noreply.github.com>
2020-09-30 09:42:25 -0400

    E2: made scaredFace image a property of scaredFace object, added comments for extra clarity of code, beautified the code

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -2,7 +2,7 @@
 Exercise 2: Dodge-em
 Sharon Ku
 
-COVID-19, represented by a red circle, will move from the left side of the canvas to the right at a random y position. Each time it reaches the right side, it will reset to the left at a random y position. The user will control their own circle with the mouse position. The user circle follows the mouse with a velocity and acceleration though, so beware! The background changes colors based on the mouse's y position. If the COVID-19 circle touches the user circle, everything stops! In the background we see random static for visual flair and we don’t see the mouse cursor.
+COVID-19, represented by a red circle, will move from the left side of the canvas to the right at a random y position. As COVID-19 moves from left to right across the screen, it follows the user circle's vertical position. Each time it reaches the right side, it will reset to the left at a random y position. The user will control their own circle with the mouse position. The user circle follows the mouse with a velocity and acceleration though, so beware! The background changes colors based on the mouse's y position. If the COVID-19 circle touches the user circle, everything stops! In the background we see random static for visual flair and we don’t see the mouse cursor.
 **************************************************/
 
 // User circle
@@ -16,19 +16,19 @@ let user = {
   ay: 0,
   acceleration: 0.06,
   size: 50,
-  fill:{
+  fill: {
     r: 250,
     g: 193,
     b: 114,
-  }
+  },
 };
 
 // Scared face on user circle
-let scaredFaceImage;
 let scaredFace = {
   x: 0,
   y: 0,
-}
+  image: undefined,
+};
 
 // Covid circle
 let covid = {
@@ -38,16 +38,14 @@ let covid = {
   vx: 0,
   vy: 0,
   speed: 3,
-  fill:{
+  fill: {
     r: 226,
     g: 91,
     b: 69,
-  }
-  // ax: 0,
-  // acceleration: 0.0007,
+  },
 };
 
-// Background
+// Background colors
 let bg = {
   r: 21,
   g: 76,
@@ -66,21 +64,12 @@ let bg = {
 let xMin = 0;
 let yMin = 0;
 
-
-
-
-
 // preload()
 //
-// Loads the scaredFace loadImage
-function preload(){
-  scaredFaceImage = loadImage("assets/images/scaredFace.png");
+// Loads the scaredFace Image
+function preload() {
+  scaredFace.image = loadImage("assets/images/scaredFace.png");
 }
-
-
-
-
-
 
 // setup()
 //
@@ -94,13 +83,9 @@ function setup() {
   noCursor();
 }
 
-
-
-
-
 // draw()
 //
-// Animating the background's color, the static effect, the covid 19 circle, the user circle, and the scared face on the user circle
+// The background's color changes, a static effect is displayed, the COVID-19 circle moves across the screen, the user circle follows the mouse, and the scared face moves with the user circle. The program stops running if COVID-19 catches the user circle.
 function draw() {
   // Background color changes based on mouse's y position
   bg.r = map(mouseY, width, yMin, bg.rMin, bg.rMax);
@@ -112,26 +97,28 @@ function draw() {
   // Display static
   push();
   for (let i = 0; i < 100; i++) {
-    let x = random(0,width);
-    let y = random(0,height);
+    let x = random(0, width);
+    let y = random(0, height);
     stroke(505);
-    point(x,y);
+    point(x, y);
   }
   pop();
 
   // Covid 19 movement
+  // x position: Goes from left to right of screen
   covid.x += covid.vx;
-
+  // y position: Follows the user circle's vertical position
   if (covid.y < user.y) {
     covid.y += covid.vy;
     covid.vy = covid.speed;
   }
-  else{
+  else {
     covid.y += covid.vy;
     covid.vy = -covid.speed;
   }
 
-  if(covid.x > width) { // once covid reaches the right side of canvas, send covid back to left side of canvas at random y position
+  // Once covid reaches the right side of canvas, send covid back to left side of canvas at random y position
+  if (covid.x > width) {
     covid.x = 0;
     covid.y = random(0, height);
   }
@@ -142,33 +129,33 @@ function draw() {
   ellipse(covid.x, covid.y, covid.size);
   pop();
 
-  // User circle movement
+  // User circle movement: follows the mouse with velocity and acceleration
   user.x = constrain(user.x, xMin, width);
   user.y = constrain(user.y, yMin, height);
 
-  if (user.x < mouseX){
+  if (user.x < mouseX) {
     user.x += user.vx;
     user.vx += user.ax;
     user.ax = user.acceleration;
   }
-  else{
+  else {
     user.x += user.vx;
     user.vx += user.ax;
     user.ax = -user.acceleration;
   }
 
-  if (user.y < mouseY){
+  if (user.y < mouseY) {
     user.y += user.vy;
     user.vy += user.ay;
     user.ay = user.acceleration;
   }
-  else{
+  else {
     user.y += user.vy;
     user.vy += user.ay;
     user.ay = -user.acceleration;
   }
 
-  // Scared face movement
+  // Scared face moves with user circle
   scaredFace.x = user.x;
   scaredFace.y = user.y;
 
@@ -178,11 +165,11 @@ function draw() {
 
   // Display scared face on user circle
   imageMode(CENTER);
-  image(scaredFaceImage, scaredFace.x, scaredFace.y);
+  image(scaredFace.image, scaredFace.x, scaredFace.y);
 
   // Check for catching Covid 19
   let d = dist(covid.x, covid.y, user.x, user.y);
-  if (d < ((covid.size/2) + (user.size/2))){
+  if (d < covid.size / 2 + user.size / 2) {
     noLoop();
   }
 }
