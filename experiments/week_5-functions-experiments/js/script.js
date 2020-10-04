c892b3906f6164f0d792d34f671f46b231ccf45f
sharon-ku <sharon-ku@users.noreply.github.com>
2020-10-04 09:59:58 -0400

    W5: learned about states

--- experiments/week_5-functions-experiments/js/script.js
+++ experiments/week_5-functions-experiments/js/script.js
@@ -5,17 +5,16 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
-let hamlet1 = "To be or not to be";
-let hamlet2 = 'That is the question';
-let hamlet3 = `Whether 'tis nobler in the mind...`;
+let circle ={
+  x:0,
+  y:250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2,
+}
 
-let hello ={
-  string: `Hello, world!`,
-  x: 0,
-  y: 0,
-  vx: 5,
-  vy: 1,
-};
+let state = `title`; // Possible states are: title, animation, ending
 
 
 // setup()
@@ -23,30 +22,54 @@ let hello ={
 // Description of setup() goes here.
 function setup() {
   createCanvas(500, 500);
+  circle.vx = circle.speed;
+  textSize(32);
+  textAlign(CENTER,CENTER);
 }
 
 function draw() {
-  background(127);
+  background(0);
 
-  textAlign(CENTER, CENTER);
-  // Make the font size respond to the mouse
-  let size = map(mouseX, 0, width, 12, 128);
-  textSize(size);
-  textStyle(BOLD);
+  if (state === `title`) {
+    title();
+  }
+  else if (state === `animation`) {
+    animation();
+  }
+  else if (state === `ending`) {
+    ending();
+  }
+}
 
-  // Make the fill respond to the mouse
-  let red = map(mouseX, 0, width, 100, 200);
-  let green = map(mouseY, 0, height, 100, 200);
-  let blue = map(mouseX + mouseY, 0, width + height, 100, 200);
-  fill(red, green, blue);
+function title(){
+  // Title
+  fill(255);
+  text(`Life.`, width/2, height/2);
+}
 
-  // Make the stroke color respond to the mouse
-  let strokeShade = map(mouseX, 0, width, 0, 255);
-  stroke(strokeShade);
+function animation(){
+  // Animation
+  circle.x += circle.vx;
+  circle.y += circle.vy;
 
-  // Make the stroke weight respond to the mouse
-  let weight = map(mouseY, 0, height, 0, 40);
-  strokeWeight(weight);
+  ellipse(circle.x, circle.y, circle.size);
+  circleIsOffScreen();
+}
 
-  text(`Hello, World!`, 250, 250);
+function ending(){
+  // Ending
+  fill(127);
+  text(`It's all over.`, width/2, height/2);
+}
+
+function keyPressed() {
+  if (state === `title`) {
+  state = `animation`;
+  }
+}
+
+function circleIsOffScreen() {
+  if (circle.x > width) {
+    state = `ending`;
+  }
 }
