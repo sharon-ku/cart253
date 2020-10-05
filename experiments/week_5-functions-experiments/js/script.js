66b393d9e328075e565f8b5b8ca5ae0946db4be0
sharon-ku <sharon-ku@users.noreply.github.com>
2020-10-05 08:20:28 -0400

    W5: learned about keyboard input

--- experiments/week_5-functions-experiments/js/script.js
+++ experiments/week_5-functions-experiments/js/script.js
@@ -5,71 +5,21 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
-let circle ={
-  x:0,
-  y:250,
-  size: 100,
-  vx: 0,
-  vy: 0,
-  speed: 2,
-}
-
-let state = `title`; // Possible states are: title, animation, ending
-
+let bg = 0;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(500, 500);
-  circle.vx = circle.speed;
-  textSize(32);
-  textAlign(CENTER,CENTER);
+
 }
 
 function draw() {
-  background(0);
+  background(bg);
 
-  if (state === `title`) {
-    title();
-  }
-  else if (state === `animation`) {
-    animation();
-  }
-  else if (state === `ending`) {
-    ending();
-  }
-}
-
-function title(){
-  // Title
-  fill(255);
-  text(`Life.`, width/2, height/2);
-}
-
-function animation(){
-  // Animation
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-
-  ellipse(circle.x, circle.y, circle.size);
-  circleIsOffScreen();
-}
-
-function ending(){
-  // Ending
-  fill(127);
-  text(`It's all over.`, width/2, height/2);
-}
-
-function keyPressed() {
-  if (state === `title`) {
-  state = `animation`;
-  }
-}
-
-function circleIsOffScreen() {
-  if (circle.x > width) {
-    state = `ending`;
+  if (keyIsDown(65)) {
+    rectMode(CENTER);
+    rect(250,250,100,100);
   }
 }
