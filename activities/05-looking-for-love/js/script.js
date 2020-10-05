046a9649ff7ff896897f06045a7c0d95511164e7
sharon-ku <sharon-ku@users.noreply.github.com>
2020-10-05 16:48:25 -0400

    A5: created intro state: displays looking for love title and start button that enlarges when mouse hovers over it

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -1,15 +1,67 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 5: Looking for love
+Sharon Ku
 
 Here is a description of this template p5 project.
 **************************************************/
+
+let state = `title`; //Possible states are: title, animation, loveTriumph, deepSadness
+
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
+
+// title text
+let titleText = `lookingForLove`;
+let titleFont;
+
+// start
+let start = {
+  text: `START`,
+  font: `Georgia`,
+  x: 100,
+  y: 100,
+  size: 35,
+  sizeBigger: 50,
+  sizeSmaller: 35,
+  // horAlign: `CENTER`,
+  // verAlign: `CENTER`,
+  fill: {
+    r: 255,
+    g: 255,
+    b: 255,
+  }
+};
+
+// start button
+let startButton = {
+  size: 200,
+  sizeBigger: 250,
+  sizeSmaller: 200,
+  x: 100,
+  y: 100,
+  fill: {
+    r: 125,
+    g: 125,
+    b: 125,
+  }
+}
+
+// loading font
+function preload(){
+  titleFont = loadFont(`assets/fonts/CinzelDecorative-Bold.ttf`);
+}
+
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 // draw()
@@ -17,4 +69,90 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
 
+  background(bg.r, bg.g, bg.b);
+
+  if (state === `title`) {
+    title();
+  }
+
+}
+
+function title() {
+  displayTitle(); // Display "Looking for Love"
+
+  displayStartButton(); // Drawing the start button
+  displayStart(); // Drawing the start text
+
+  enlargeStartButton(); // Start button and Start text enlarge if mouse's position is on start button
+}
+
+// The Start button and Start text enlarge if mouse's position is on start button
+function enlargeStartButton() {
+  if (mouseIsInStartButton()) {
+    startButton.size = startButton.sizeBigger;
+    start.size = start.sizeBigger;
+  }
+  else {
+    startButton.size = startButton.sizeSmaller;
+    start.size = start.sizeSmaller;
+  }
+}
+
+function mouseClicked() {
+  if (mouseIsInStartButton()) {
+    startButton.fill.r = 70;
+    state = `animation`;
+  }
+}
+
+// Checks if mouse's position is inside the start button
+function mouseIsInStartButton() {
+  if (mouseX < startButton.x+(startButton.size/2) && mouseX > startButton.x-(startButton.size/2)) {
+    if (mouseY < startButton.y+(startButton.size/2) && mouseY > startButton.y-(startButton.size/2)) {
+      return true;
+    }
+  }
+  else{
+    return false;
+  }
+}
+
+// Display title "Looking for Love"
+function displayTitle() {
+  push();
+  fill(255);
+  textSize(windowHeight/7);
+  textAlign(CENTER,CENTER);
+
+  textFont(titleFont);
+  text(`Looking`, windowWidth/3, windowHeight/3);
+  text(`for`, windowWidth/2, windowHeight/2);
+  text(`Love`, windowWidth*2/3, windowHeight*2/3);
+  pop();
+}
+
+// Display the circular Start button
+function displayStartButton() {
+  push();
+  startButton.x = start.x;
+  startButton.y = start.y;
+
+  fill(startButton.fill.r, startButton.fill.g, startButton.fill.b);
+  ellipse(startButton.x, startButton.y, startButton.size);
+  pop();
+}
+
+// Display the Start text
+function displayStart() {
+  push();
+  fill(start.fill.r, start.fill.g, start.fill.b);
+  textSize(start.size);
+  textAlign(CENTER, CENTER);
+
+  start.x = windowWidth*1/5;
+  start.y = windowHeight*4/5;
+
+  textFont(start.font);
+  text(start.text, start.x, start.y);
+  pop();
 }
