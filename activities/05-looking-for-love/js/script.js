c935ecf64402d35ac2783d375353c6caf01a0c9b
sharon-ku <sharon-ku@users.noreply.github.com>
2020-10-07 22:38:37 -0400

    A5: added two ending states: loveTriumph and deepSadness

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -11,6 +11,9 @@ let bg = {
   r: 0,
   g: 0,
   b: 0,
+  rSad: 81,
+  gSad: 26,
+  bSad: 168,
 };
 
 // VARIABLES FOR THE INTRO -------------------------------------------------------------
@@ -25,14 +28,14 @@ let start = {
   x: 100,
   y: 100,
   size: 35,
-  sizeBigger: 50,
+  sizeBigger: 40,
   sizeSmaller: 35,
   // horAlign: `CENTER`,
   // verAlign: `CENTER`,
   fill: {
-    r: 255,
-    g: 255,
-    b: 255,
+    r: 0,
+    g: 0,
+    b: 0,
   }
 };
 
@@ -44,9 +47,12 @@ let startButton = {
   x: 100,
   y: 100,
   fill: {
-    r: 125,
-    g: 125,
-    b: 125,
+    r: 255, // light pink
+    g: 205,
+    b: 255,
+    rHover: 10, // vivid sky blue
+    gHover: 205,
+    bHover: 255,
   }
 };
 
@@ -63,9 +69,13 @@ let circle1 = {
   txChange: 0.025,
   tyChange: 0.025,
   fill: {
-    r: 255,
-    g: 0,
-    b: 0,
+    r: 213, // flashy purple
+    g: 77,
+    b: 247,
+    rLove: 180, // lavender floral
+    gLove: 139,
+    bLove: 231,
+    changeSpeed: 2,
   }
 };
 
@@ -81,9 +91,13 @@ let circle2 = {
   txChange: 0.015,
   tyChange: 0.015,
   fill: {
-    r: 0,
-    g: 255,
-    b: 0,
+    r: 28, //sea green crayola
+    g: 234,
+    b: 182,
+    rLove: 255, // baker miller pink
+    gLove: 148,
+    bLove: 180,
+    changeSpeed: 2,
   }
 };
 
@@ -127,47 +141,101 @@ function draw() {
     animation();
   }
 
+  if (state === `loveTriumph`) {
+    loveTriumph();
+  }
+
+  if (state === `deepSadness`) {
+    deepSadness();
+  }
 }
 
 function title() {
   displayTitle(); // Display "Looking for Love"
-
   displayStartButton(); // Drawing the start button
   displayStart(); // Drawing the start text
-
-  enlargeStartButton(); // Start button and Start text enlarge if mouse's position is on start button
+  hoverOnStartButton(); // Start button and Start text enlarge if mouse's position is on start button
 }
 
 function animation(){
-  // displays and moves 2 circles
+  // Displays and moves 2 circles
   moveCircle1(circle1.txChange, circle1.tyChange, circle1.vx, circle1.vy, circle1.speed);
   displayCircle(circle1.fill.r, circle1.fill.g, circle1.fill.b, circle1.x, circle1.y, circle1.size);
 
   moveCircle2(circle2.txChange, circle2.tyChange, circle2.vx, circle2.vy, circle2.speed);
   displayCircle(circle2.fill.r, circle2.fill.g, circle2.fill.b, circle2.x, circle2.y, circle2.size);
 
+  // Cue loveTriumph if circles touch
+  if (circlesTouch()) {
+    state = `loveTriumph`;
+  }
+
   // Cue deepSadness if either circle goes off canvas
+  if (circleGoesOffCanvas()) {
+    state = `deepSadness`;
+  }
+}
 
+function loveTriumph() {
+  // display circle 1 with different fill color
+  if (circle1.fill.r < circle1.fill.rLove) {
+    circle1.fill.r += circle1.fill.changeSpeed;
+    circle1.fill.r = constrain(circle1.fill.r, 0, circle1.fill.rLove);
+  }
+  else {
+    circle1.fill.r -= circle1.fill.changeSpeed;
+    circle1.fill.r = constrain(circle1.fill.r, circle1.fill.rLove, 255);
+  }
+  if (circle1.fill.g < circle1.fill.gLove) {
+    circle1.fill.g += circle1.fill.changeSpeed;
+    circle1.fill.g = constrain(circle1.fill.g, 0, circle1.fill.gLove);
+  }
+  else {
+    circle1.fill.g -= circle1.fill.changeSpeed;
+    circle1.fill.g = constrain(circle1.fill.g, circle1.fill.gLove, 255);
+  }
 
+  displayCircle(circle1.fill.r, circle1.fill.gLove, circle1.fill.bLove, circle1.x, circle1.y, circle1.size);
+
+  //display circle 2 with different fill color
+
+  displayCircle(circle2.fill.rLove, circle2.fill.gLove, circle2.fill.bLove, circle2.x, circle2.y, circle2.size);
+}
+
+function deepSadness() {
+  background(bg.rSad, bg.gSad, bg.bSad);
 
 
 }
 
-
 // FUNCTIONS -----------------------------------------------------------------------------
 
-// The Start button and Start text enlarge if mouse's position is on start button
-function enlargeStartButton() {
+// Behavior of Start button and Start text when mouse hovers over button
+function hoverOnStartButton() {
   if (mouseIsInStartButton()) {
+    push();
+    // Start button enlarges and changes color
     startButton.size = startButton.sizeBigger;
+    fill(startButton.fill.rHover, startButton.fill.gHover, startButton.fill.bHover);
+    ellipse(startButton.x, startButton.y, startButton.size);
+
+    // Start text enlarges
     start.size = start.sizeBigger;
+    fill(start.fill.r, start.fill.g, start.fill.b);
+    textAlign(CENTER, CENTER);
+    textSize(start.size);
+    textFont(start.font);
+    text(start.text, start.x, start.y);
+    pop();
   }
   else {
+    // Start button and text look the same as before
     startButton.size = startButton.sizeSmaller;
     start.size = start.sizeSmaller;
   }
 }
 
+// If user clicks on Start button, cue `animation` state
 function mouseClicked() {
   if (mouseIsInStartButton()) {
     startButton.fill.r = 70;
@@ -263,4 +331,28 @@ function moveCircle2(txChange, tyChange, vx, vy, speed){
 
   circle2.x += vx;
   circle2.y += vy;
+}
+
+// Check if circle goes off canvas
+function circleGoesOffCanvas() {
+  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Check if circles touch
+function circlesTouch(){
+  // Calculate distance between circles
+  let distBtwCircles = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+
+  // Return true if circles touch
+  if (distBtwCircles < (circle1.size/2) + (circle2.size/2)) {
+    return true;
+  }
+  else{
+    return false;
+  }
 }
