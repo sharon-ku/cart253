f8f05ee7be4d7070338a2bc15260245cd2a8e0d8
sharon-ku <sharon-ku@users.noreply.github.com>
2020-10-09 18:32:24 -0400

    P1: firefish flips direction when following finger

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -17,7 +17,7 @@ let firefish = {
   vx: 0,
   vy: 0,
   speed: {
-    casualSwimming: 3,
+    casualSwimming: 5,
     followingMouse: 0.8,
   },
   tx: 0,
@@ -31,6 +31,7 @@ let firefish = {
     y: 1,
   },
   angle: 0,
+  finalAngle: 90,
 };
 
 // User circle
@@ -55,7 +56,7 @@ let bg = {
 };
 
 let fishtank = {
-  border: 50,
+  border: 200,
 };
 
 // Preload fish images
@@ -96,21 +97,12 @@ function intro() {
   // Firefish follows finger if the fish senses the finger, or else it swims casually around the tank.
   if (firefishSensesFinger()) {
     fishFollowsFinger({x: firefish.x, y: firefish.y, vx: firefish.vx, vy: firefish.vy, speed: firefish.speed.followingMouse});
-
-    // Trying to figure out how to rotate fish
-    push();
-    // translate(firefish.x, firefish.y);
-    firefish.angle = PI/4;
-    pop();
   }
   else {
     firefishCasualSwimming();
   }
 
-  // Trying to figure out how to flip fish
-  // if (firefish.x < mouseX) {
-  //   firefish.width *= -1;
-  // }
+
 }
 
 
@@ -118,21 +110,37 @@ function intro() {
 function displayFinger() {
   finger.x = mouseX;
   finger.y = mouseY;
-  fill(finger.fill.r, finger.fill.g, finger.fill.b, finger.fill.alpha);
 
+  push();
+  fill(finger.fill.r, finger.fill.g, finger.fill.b, finger.fill.alpha);
   ellipse(finger.x, finger.y, finger.size);
+  pop();
 }
 
 // The fish follows the finger
 function fishFollowsFinger({x, y, vx, vy, speed}) {
-  push();
-  let distX = firefish.x - mouseX;
+  let distX;
   let distY = firefish.y - mouseY;
+
+  if (firefish.x < mouseX) {
+    // distX = firefish.x - mouseX + firefish.length/2;
+    distX = firefish.x - mouseX;
+  }
+  else {
+    // distX = firefish.x - mouseX - firefish.length/2;
+    distX = firefish.x - mouseX;
+  }
+
+  // push();
+  // translate(firefish.x,firefish.y);
+  // setFishAngle();
+  // pop();
+
 
   if (distX < 0) {
     vx = speed;
   }
-  else if (distX > 0) {
+  else {
     vx = -speed;
   }
 
@@ -143,10 +151,15 @@ function fishFollowsFinger({x, y, vx, vy, speed}) {
     vy = -speed;
   }
 
+  if (vx > 0) {
+    firefish.scale.x = -1;
+  }
+  else {
+    firefish.scale.x = 1;
+  }
+
   firefish.x += vx;
   firefish.y += vy;
-
-  pop();
 }
 
 // Returns true if finger is close enough to the fish (within the fish's field of vision)
@@ -161,38 +174,63 @@ function firefishSensesFinger(){
 
 // Firefish swims randomly using Perlin noise
 function firefishCasualSwimming(){
-  push();
-  // translate(firefish.x,firefish.y);
   firefish.tx += firefish.txChange;
   firefish.ty += firefish.tyChange;
 
   let noiseX = noise(firefish.tx);
   let noiseY = noise(firefish.ty);
 
+  let change = random();
+
+  if (change < 0.02) {
   firefish.vx = map(noiseX, 0, 1, -firefish.speed.casualSwimming, firefish.speed.casualSwimming);
   firefish.vy = map(noiseY, 0, 1, -firefish.speed.casualSwimming, firefish.speed.casualSwimming);
+  }
 
   firefish.x += firefish.vx;
   firefish.y += firefish.vy;
 
-  firefish.x = constrain(firefish.x, 0, width);
-  firefish.y = constrain(firefish.y, 0, height);
-  pop();
+  firefish.x = constrain(firefish.x, fishtank.border, width - fishtank.border);
+  firefish.y = constrain(firefish.y, fishtank.border, height - fishtank.border);
 }
 
 // Display firefish
 function displayFirefish({img, x, y, length, width}) {
   push();
   imageMode(CENTER);
-  // Trying to figure out how to flip fish
-  // translate(firefish.x, firefish.y);
-  // scale(firefish.scale.x, firefish.scale.y);
 
-  // Trying to figure out how to rotate fish
-  translate(firefish.x,firefish.y);
-  rotate(firefish.angle);
-  firefish.angle += 0.0215; // made an animation to see if fish rotates properly: turns out it does not :(
-  image(img, x, y, length, width);
+  translate(x,y);
+
+  setFishAngle();
+
+  scale(firefish.scale.x, firefish.scale.y);
+  setFishDirection(firefish.x,firefish.y); // Fish faces direction it is swimming
+  image(img, 0, 0, length, width);
   pop();
+}
 
+function setFishAngle() {
+  rotate(firefish.angle);
+  firefish.finalAngle = atan(firefish.vx,firefish.vy) / 3;
+
+  if (firefish.angle < firefish.finalAngle) {
+    firefish.angle += 0.0005;
+  }
+  else {
+    firefish.angle -= 0.0005;
+  }
+  console.log(firefish.angle);
+}
+
+// Fish faces direction it is swimming
+function setFishDirection(x,y) {
+  push();
+  translate(x,y);
+  if (firefish.vx > 0) {
+    firefish.scale.x = -1;
+  }
+  else {
+    firefish.scale.x = 1;
+  }
+  pop();
 }
