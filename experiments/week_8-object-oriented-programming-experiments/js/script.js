ed8ad729a0bf525a229d6f2af4a9bc89d18fa0bc
sharon-ku <sharon-ku@users.noreply.github.com>
2020-10-23 08:47:33 -0400

    W8: learned abt object-oriented programming (oop)

---
+++ experiments/week_8-object-oriented-programming-experiments/js/script.js
@@ -0,0 +1,34 @@
+/**************************************************
+Week 8: Object-oriented programming
+Sharon Ku
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// Our garden
+let garden = {
+  // An array to store the individual flowers
+  flowers: [],
+  // How many flowers in the garden
+  numFlowers: 20,
+  // The color of the grass (background)
+  grassColor: {
+    r: 120,
+    g: 180,
+    b: 120
+  }
+};
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
