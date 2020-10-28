bb02090be917c719c510de641c7c5d315d998395
sharon-ku <sharon-ku@users.noreply.github.com>
2020-10-28 10:22:44 -0400

    W8.4 learned about interacting objects with bee and flower garden example

--- experiments/week_8-object-oriented-programming-experiments/js/Flower.js
+++ experiments/week_8-object-oriented-programming-experiments/js/Flower.js
@@ -1,13 +1,16 @@
 class Flower {
+
   // The constructor() sets up a flower's properties
   constructor(x, y, size, stemLength, petalColor) {
     // Position and size information
     this.x = x;
     this.y = y;
     this.size = size;
+    this.maxSize = size; // NEW! To limit growth
     this.stemLength = stemLength;
     this.stemThickness = 10;
     this.petalThickness = 10;
+    this.maxPetalThickness = 10; // NEW! To limit growth
     // Color information
     this.stemColor = {
       r: 50,
@@ -20,6 +23,37 @@ class Flower {
       g: 0,
       b: 0
     };
+    this.alive = true;
+  }
+
+  // shrink()
+  // Shrinks the flower
+  shrink() {
+    // Choose a random amount to shrink
+    let shrinkage = random(0, 0.1);
+    // Reduce the petal thickness (divide by 10 to make it less rapid)
+    this.petalThickness = this.petalThickness - shrinkage / 10;
+    // Reduce the centre of the flower
+    this.size = this.size - shrinkage;
+
+    // If any of the key properties reach 0 or less, the flower is dead
+    if (this.petalThickness <= 0 || this.size <= 0) {
+      this.alive = false;
+    }
+  }
+
+  // NEW! pollinate() handles the flower being pollinated (it grows)
+  pollinate() {
+    // Choose a random amount to grow
+    let growth = random(0, 0.5);
+    // Increase the petal thickness (divide by 10 to make it less rapid)
+    this.petalThickness = this.petalThickness + growth / 10;
+    // Increase the centre of the flower
+    this.size = this.size + growth;
+
+    // Constrain the elements
+    this.petalThickness = constrain(this.petalThickness, 0, this.maxPetalThickness);
+    this.size = constrain(this.size, 0, this.maxSize);
   }
 
   // display()
--- experiments/week_8-object-oriented-programming-experiments/js/script.js
+++ experiments/week_8-object-oriented-programming-experiments/js/script.js
@@ -1,10 +1,3 @@
-/**************************************************
-Week 8: Object-oriented programming
-Sharon Ku
-
-Here is a description of this template p5 project.
-**************************************************/
-
 "use strict";
 
 // Our garden
@@ -12,7 +5,11 @@ let garden = {
   // An array to store the individual flowers
   flowers: [],
   // How many flowers in the garden
-  numFlowers: 20,
+  numFlowers: 100,
+  // An array to our the bees
+  bees: [],
+  // How many bees in the garden
+  numBees: 200,
   // The color of the grass (background)
   grassColor: {
     r: 120,
@@ -42,8 +39,19 @@ function setup() {
     // Add the flower to the array of flowers
     garden.flowers.push(flower);
   }
-}
 
+  // Create our bees by counting up to the number of bees
+  for (let i = 0; i < garden.numBees; i++) {
+    // Create variables for our arguments for clarity
+    let x = random(0, width);
+    let y = random(0, height);
+    // Create a new bee using the arguments
+    let bee = new Bee(x, y);
+    // Add the bee to the array of bees
+    garden.bees.push(bee);
+  }
+
+}
 
 // draw()
 // Displays our flowers
@@ -54,6 +62,34 @@ function draw() {
   // Loop through all the flowers in the array and display them
   for (let i = 0; i < garden.flowers.length; i++) {
     let flower = garden.flowers[i];
-    flower.display();
+    // Check if this flower is alive
+    if (flower.alive) {
+      // Update the flower by shrinking it and displaying it
+      flower.shrink();
+      flower.display();
+    }
   }
+
+  // Loop through all the bees in the array and display them
+  for (let i = 0; i < garden.bees.length; i++) {
+    let bee = garden.bees[i];
+    // Check if this flower is alive
+    if (bee.alive) {
+      // Shrink and move the bee
+      bee.shrink();
+      bee.move();
+
+      // NEW! Go through the entire flower array and try to pollinate the flowers!
+      // Note that we use j in our for-loop here because we're already inside
+      // a for-loop using i!
+      for (let j = 0; j < garden.flowers.length; j++) {
+        let flower = garden.flowers[j];
+        bee.tryToPollinate(flower);
+      }
+
+      // Display the bee
+      bee.display();
+    }
+  }
+
 }
