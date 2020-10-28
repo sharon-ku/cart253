73f80624649cad9414aaf59f22d55a376fc237b8
sharon-ku <sharon-ku@users.noreply.github.com>
2020-10-28 10:11:26 -0400

    W8.2 learned abt oop with constructor parameters

--- experiments/week_8-object-oriented-programming-experiments/js/script.js
+++ experiments/week_8-object-oriented-programming-experiments/js/script.js
@@ -5,6 +5,8 @@ Sharon Ku
 Here is a description of this template p5 project.
 **************************************************/
 
+"use strict";
+
 // Our garden
 let garden = {
   // An array to store the individual flowers
@@ -19,16 +21,39 @@ let garden = {
   }
 };
 
-// setup()
-//
-// Description of setup() goes here.
+// setup() creates the canvas and the flowers in the garden
 function setup() {
+  createCanvas(600, 600);
 
+  // Create our flowers by counting up to the number of the flowers
+  for (let i = 0; i < garden.numFlowers; i++) {
+    // Create variables for our arguments for clarity
+    let x = random(0, width);
+    let y = random(0, height);
+    let size = random(50, 80);
+    let stemLength = random(50, 100);
+    let petalColor = {
+      r: random(100, 255),
+      g: random(100, 255),
+      b: random(100, 255)
+    }
+    // Create a new flower using the arguments
+    let flower = new Flower(x, y, size, stemLength, petalColor);
+    // Add the flower to the array of flowers
+    garden.flowers.push(flower);
+  }
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
 
+// draw()
+// Displays our flowers
+function draw() {
+  // Display the grass
+  background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+
+  // Loop through all the flowers in the array and display them
+  for (let i = 0; i < garden.flowers.length; i++) {
+    let flower = garden.flowers[i];
+    flower.display();
+  }
 }
