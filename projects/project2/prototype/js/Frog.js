02c6ee3d2a8985cd211b0a501e5851a315eb9169
sharon-ku <sharon-ku@users.noreply.github.com>
2020-11-04 17:40:30 -0500

    P2 proposal: imported frog images, created Frog.js class

---
+++ projects/project2/prototype/js/Frog.js
@@ -0,0 +1,16 @@
+class Frog {
+  constructor() {
+
+  }
+
+  // displays frog image
+  display() {
+    imageMode(CENTER);
+    image(frogImages[0], width/2, height/2);
+  }
+
+  // move frog using arrow keys
+  move() {
+
+  }
+}
--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -1,20 +1,51 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Project 2: Prototype
+Sharon Ku
 
 Here is a description of this template p5 project.
 **************************************************/
+
+"use strict";
+
+// using an array to store all images
+let frogImages = [];
+// this variable stores the number of images
+let numFrogImages = 2;
+
+// preload()
+//
+// loads all images + sound effects
+function preload() {
+  // for (let i=0; i<numImages; i++) {
+  //   let loadedImage = loadImage(`assets/images/frog-drawings/frog-${i}.png`);
+  //   images.push(loadedImage);
+  // }
+
+  frogImages[0] = loadImage(`assets/images/frog-drawings/frog-1.png`);
+  frogImages[1] = loadImage(`assets/images/frog-drawings/frog-2.png`);
+  frogImages[2] = loadImage(`assets/images/frog-drawings/frog-3.png`);
+  frogImages[3] = loadImage(`assets/images/frog-drawings/frog-4.png`);
+
+
+}
+
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
+
+
+
+
 
 }
