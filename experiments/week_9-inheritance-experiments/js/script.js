51c2cf47172c4fad8919a91362c872a78c61577d
sharon-ku <sharon-ku@users.noreply.github.com>
2020-11-08 13:14:09 -0500

    W9.3: learned about polymorphism with traffic simulation

--- experiments/week_9-inheritance-experiments/js/script.js
+++ experiments/week_9-inheritance-experiments/js/script.js
@@ -7,13 +7,8 @@ Here is a description of this template p5 project.
 
 "use strict";
 
-
-// Our cars
-let cars = [];
-let numCars = 5;
-
-// Our motorcycles
-let motorcycles = [];
+let vehicles = [];
+let numCars = 10;
 let numMotorcycles = 10;
 
 // Set up the canvas and our cars
@@ -27,7 +22,7 @@ function setup() {
     let x = random(0, width);
     let y = random(0, height);
     let car = new Car(x, y);
-    cars.push(car);
+    vehicles.push(car);
   }
 
   // Create the correct number of motorcycles and put them in our array
@@ -35,7 +30,7 @@ function setup() {
     let x = random(0, width);
     let y = random(0, height);
     let motorcycle = new Motorcycle(x, y);
-    motorcycles.push(motorcycle);
+    vehicles.push(motorcycle);
   }
 }
 
@@ -43,22 +38,13 @@ function setup() {
 function draw() {
   background(0);
 
-  // Go through all the cars and move, wrap, and display them
-  for (let i = 0; i < cars.length; i++) {
-    let car = cars[i];
-    car.move();
-    car.wrap();
-    car.display();
-  }
 
 
 
-
-  // Go through all the motorcycles and move, wrap, and display them
-  for (let i = 0; i < motorcycles.length; i++) {
-    let motorcycle = motorcycles[i];
-    motorcycle.move();
-    motorcycle.wrap();
-    motorcycle.display();
+  for (let i=0; i<vehicles.length; i++) {
+    let vehicle = vehicles[i];
+    vehicle.move();
+    vehicle.wrap();
+    vehicle.display();
   }
 }
