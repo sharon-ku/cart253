772009a82ff8c051907174eb3afb2738eb4a85d8
sharon-ku <sharon-ku@users.noreply.github.com>
2020-11-10 18:29:55 -0500

    W10.5 learned about p5.AudioIn with scared ghost ex

--- experiments/week_10-p5-sound-experiments/js/script.js
+++ experiments/week_10-p5-sound-experiments/js/script.js
@@ -6,19 +6,31 @@ Here is a description of this template p5 project.
 **************************************************/
 "use strict";
 
+let mic;
 
-let synth;
-let notes = [`F2`, `G2`, `Ab3`, `Bb3`, `C3`, `Db3`, `Eb3`, `F3`]; //F-minor
-let currentNote = 0;
+let ghost = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  image: undefined
+};
+
+function preload() {
+  ghost.image = loadImage(`assets/images/clown.png`);
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(600,600);
-  userStartAudio();
 
-  synth = new p5.PolySynth();
+  ghost.x = width/2;
+  ghost.y = height/2;
+
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 // draw()
@@ -26,19 +38,31 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
-}
 
-function keyPressed() {
-  // start the ghost player
-  setInterval(playRandomNote, 500);
-}
 
-function playRandomNote() {
-  let note = notes[currentNote];
-  synth.play(note, 1,0,0.1);
+  // trembling
+  ghost.x += random(-1,1);
+  ghost.y += random(-1,1);
 
-  currentNote +=1;
-  if (currentNote === notes.length) {
-    currentNote = 0;
+
+  // get volume into microphone
+  let level = mic.getLevel();
+
+  // check if the ghost is scared
+  if (level>0.6) {
+    // exit stage right!
+    ghost.vx = 20;
   }
+
+  // move the ghost
+  ghost.x += ghost.vx;
+  ghost.y += ghost.vy;
+
+  // display ghost
+  push();
+  imageMode(CENTER);
+  tint(255,50);
+  image(ghost.image, ghost.x, ghost.y);
+  pop();
+
 }
