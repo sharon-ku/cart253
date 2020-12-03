39df7b4e22e8788d786d7ce1a317b9ccddfb099d
sharon-ku <sharon-ku@users.noreply.github.com>
2020-12-02 22:42:46 -0500

    P2: change gobyby's size (shrunk it to 5/6 of its previous size)

--- projects/project2/js/fishies/Goby.js
+++ projects/project2/js/fishies/Goby.js
@@ -2,8 +2,8 @@ class Goby extends Fish {
   constructor(fishImg1, fishImg2) {
     super(fishImg1, fishImg2)
     // size information
-    this.length = 130;
-    this.width = 39;
+    this.length = 108.3; //original size of image: 130
+    this.width = 32.5; //original size of image: 39
     // movement information
     this.speed = {
       casualSwimming: 6,
